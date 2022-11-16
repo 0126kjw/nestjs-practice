08f44c5c884c2a1b8b4c14bd96573ca78e0c2b12
Jinwoo Kim <0126kjw@kakao.com>
2022-11-16 23:20:09 +0900

    Update create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,23 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
+
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
