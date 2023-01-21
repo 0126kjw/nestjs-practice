0806dc4f3265fd3861458c8b9c1cc5f028dc48bd
Jinwoo Kim <0126kjw@kakao.com>
2023-01-21 21:08:59 +0900

    [socket.io] Add redis adapter

--- socket.io/src/main.ts
+++ socket.io/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useWebSocketAdapter(new IoAdapter(app));
+
+  // Uncomment these lines to use the Redis adapter:
+  // const redisIoAdapter = new RedisIoAdapter(app);
+  // await redisIoAdapter.connectToRedis();
+  // app.useWebSocketAdapter(redisIoAdapter);
+
   await app.listen(3000);
 }
 bootstrap();
