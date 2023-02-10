23861d35b6df7a14ef9c37dc013ddac11d8e4709
Jinwoo Kim <0126kjw@kakao.com>
2023-02-11 00:01:02 +0900

    [chat] Fix: socket Error

--- chat/src/chat.gateway.ts
+++ chat/src/chat.gateway.ts
@@ -5,12 +5,13 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
 
 @WebSocketGateway(81, { namespace: 'chat' })
 export class ChatGateway {
   @WebSocketServer()
-  server: Server;
+  server;
+
+  wsClients = [];
 
   @SubscribeMessage('hihi')
   connectSomeone(@MessageBody() data: string, @ConnectedSocket() client) {
@@ -18,11 +19,14 @@ export class ChatGateway {
     console.log(`${nickname}님이 코드: ${room}방에 접속했습니다.`);
     const comeOn = `${nickname}님이 입장했습니다.`;
     this.server.emit('comeOn' + room, comeOn);
+    this.wsClients.push(client);
   }
 
   private broadcast(event, client, message: any) {
-    for (let id in this.server.sockets)
-      if (id !== client.id) this.server.sockets[id].emit(event, message);
+    for (let c of this.wsClients) {
+      if (client.id == c.id) continue;
+      c.emit(event, message);
+    }
   }
 
   @SubscribeMessage('send')
